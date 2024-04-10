3a3a0811f526e50fa4063b923906e168c303fb1d
Seongwon Bang <bsw9698@gmail.com>
2024-04-10 22:32:17 +0900

    feat: 학생 인증 기능 구현

--- src/main/domain/Customer.java
+++ src/main/domain/Customer.java
@@ -1,5 +1,9 @@
 package main.domain;
 
+import main.Ojdbc;
+
+import java.sql.SQLException;
+
 public class Customer extends Member{
 	public void order() {
 
@@ -9,8 +13,42 @@ public class Customer extends Member{
 
 	}
 
-	public void authorizeStudent() {
+	public void authorizeStudent(Customer customer) {
+		if("student".equals(customer.getMember_type())){
+			System.out.println("이미 학생 인증을 마치셨습니다");
+			return;
+		}
+		System.out.println("--------------------------------------------------------");
+		System.out.print("학생(Y/N): ");
+		String status = Ojdbc.sc.nextLine();
 
+		try {
+			String sql = new StringBuilder()
+					.append("UPDATE member SET ")
+					.append("member_type =? ")
+					.append("WHERE id =?")
+					.toString();
+			Ojdbc.pstmt = Ojdbc.conn.prepareStatement(sql);
+			if ("Y".equals(status)) {
+				Ojdbc.pstmt.setString(1, "student");
+				customer.setMember_type("student");
+			} else if ("N".equals(status)) {
+				Ojdbc.pstmt.setString(1, "nonstudent");
+				customer.setMember_type("nonstudent");
+			} else {
+				throw new Exception();
+			}
+			Ojdbc.pstmt.setString(2, customer.getId());
+
+			Ojdbc.pstmt.executeUpdate();
+
+			System.out.println("학생 인증에 성공하였습니다.");
+			Ojdbc.pstmt.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
 	}
 
 
